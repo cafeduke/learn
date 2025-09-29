05169fcd9ec8d407b6d761803c233d974d2f63bc
Raghunandan.Seshadri <raghubs81@gmail.com>
2025-09-29 13:49:53 +0530

    SpringMicro: Rename service

--- JavaEE/Spring/WebService/L01_RESTWebServicevice/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into user values (101, sysdate(), 'Hari');
-insert into user values (102, sysdate(), 'Harsha');
-insert into user values (103, sysdate(), 'Sanjay');
-
-insert into post values (201, 'Hari first post', 101);
-insert into post values (202, 'Hari scond post', 101);
